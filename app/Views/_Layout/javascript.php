6a6ecb272f0f1c1f7794b3db9476f4098e87fecb
michael <michaelfebr5@gmail.com>
2023-06-20 10:35:38 +0800

    Commit template 2

--- app/Views/_Layout/javascript.php
+++ app/Views/_Layout/javascript.php
@@ -1,5 +1,5 @@
-<script src="/template/dist/assets/static/js/initTheme.js"></script>
-<script src="/template/dist/assets/static/js/components/dark.js"></script>
-<script src="/template/dist/assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="node_modules/assets/static/js/initTheme.js"></script>
+<script src="node_modules/assets/static/js/components/dark.js"></script>
+<script src="node_modules/assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 
-<script src="/template/dist/assets/compiled/js/app.js"></script>
+<script src="node_modules/assets/compiled/js/app.js"></script>
