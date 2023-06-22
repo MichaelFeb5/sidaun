cbbb933ae16eb0ef7dd1ae4503921754fa30bc06
michael <michaelfebr5@gmail.com>
2023-06-22 22:11:32 +0800

    Tambah Fitur Registrasi

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -8,4 +8,20 @@ class Login extends BaseController
     {
         return view('Login/index.php');
     }
+
+    public function simpan()
+    {
+        // Insisialisasi Model
+        $tb_akun = new \App\Models\tb_akun();
+
+        // Take Data from view
+        $data = ($this->request->getPost('model') ? $this->request->getPost('model') : []);
+        $res = $tb_akun->insert($data);
+        return $this->response->setJSON($res);
+    }
+
+    public function registrasi()
+    {
+        return view('Registrasi/index.php');
+    }
 }
